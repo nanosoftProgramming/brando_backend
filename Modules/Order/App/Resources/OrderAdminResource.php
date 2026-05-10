a26c4a58c24b8929faf01b68ab8e4e41ef1a3623
NancyAhmed97 <anancyahmed97@icloud.com>
2026-05-10 17:55:05 +0300

    edit order resouces

--- Modules/Order/App/Resources/OrderAdminResource.php
+++ Modules/Order/App/Resources/OrderAdminResource.php
@@ -10,21 +10,26 @@ class OrderAdminResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'status' => $this->status->name,
+
+            'status' => $this->status?->name,
+
             'payment_type' => $this->payment_type,
-            'total_items' => $this->items->count(),
+
+            'total_items' => $this->items?->count() ?? 0,
 
             'total_price' => $this->total,
             'sub_total' => $this->sub_total,
             'taxes' => $this->taxes,
 
-            'created_at' => $this->created_at->format('Y-m-d h:i A'),
+            'created_at' => optional($this->created_at)->format('Y-m-d h:i A'),
+
             'client' => [
                 'id' => $this->client?->id,
                 'name' => $this->client?->name,
                 'email' => $this->client?->email,
                 'phone' => $this->client?->phone,
             ],
+
             'address' => [
                 'id' => $this->address?->id,
                 'title' => $this->address?->title,
@@ -35,11 +40,13 @@ class OrderAdminResource extends JsonResource
                 'city' => $this->address?->city?->name,
                 'phone' => $this->address?->phone?->phone,
             ],
+
             'branch' => [
                 'id' => $this->branch?->id,
                 'name' => $this->branch?->name,
             ],
-            'items' => $this->items->map(function ($item) {
+
+            'items' => $this->items?->map(function ($item) {
                 return [
                     'id' => $item->id,
                     'quantity' => $item->quantity,
@@ -51,7 +58,8 @@ class OrderAdminResource extends JsonResource
                         'image' => $item->branch_product?->product?->image,
                     ],
                 ];
-            }),
+            }) ?? [],
+
             'rate' => $this->whenLoaded('rate'),
         ];
     }
